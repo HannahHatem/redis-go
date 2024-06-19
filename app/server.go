b131dcdc8ac65c1233ef3599362c3c84900d56df
Hannah Hatem <hanahatem.taha@yahoo.com>
2024-06-19 21:41:22 +0300

    establish TCP connection and respond to PING

--- app/server.go
+++ app/server.go
@@ -1,26 +1,59 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
-	// Uncomment this block to pass the first stage
+	"log"
 	"net"
 	"os"
 )
+
+var listen = flag.String("listen", ":6379", "listen address")
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	flag.Parse()
+
+	err := run()
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Error running server", err.Error())
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+}
+
+func run() error {
+	l, err := net.Listen("tcp", *listen)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Failed to bind to port 6379", err.Error())
+		return errors.New("failed to bind to port 6379")
 	}
+	defer l.Close()
+
+	c, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection", err.Error())
+		return errors.New("error accepting connection")
+	}
+
+	defer c.Close()
+
+	buf := make([]byte, 128)
+	_, err = c.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection", err.Error())
+		return errors.New("error reading from connection")
+	}
+
+	log.Printf("received from c.Read(buf): %s", buf)
+
+	_, err = c.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection c.Write()", err.Error())
+		return errors.New("error writing to connection")
+	}
+
+	return nil
 }
